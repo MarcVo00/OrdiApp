1995f4d04ebc221f3a7e67f283f5b48ef083dd7c
MarcVO <ductungmarcvo@gmail.com>
2025-07-23 15:12:12 +0200

    add expo

--- bar-app/app/_layout.tsx
+++ bar-app/app/_layout.tsx
@@ -1,11 +1,13 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import {Slot} from 'expo-router';
 import { CartProvider } from './context/Cartcontext';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useFonts } from 'expo-font';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 export default function Layout() {
   return (
